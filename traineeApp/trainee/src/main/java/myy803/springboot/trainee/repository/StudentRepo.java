79cd9b2934e38fc217256e0e5532cde08f97af98
EliasGrats <elgrats@outlook.com>
2025-05-12 18:49:37 +0300

    kauliarides

---
+++ traineeApp/trainee/src/main/java/myy803/springboot/trainee/repository/StudentRepo.java
@@ -0,0 +1,10 @@
+package myy803.springboot.trainee.repository;
+import java.util.Optional;
+import myy803.springboot.trainee.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepo extends JpaRepository<Student, Integer> {
+
+    Optional<Student> findByUsername(String username);
+
+}
