69c9f3185b2238909f4fe070d520759c4cfe186b
DimitrisTzalo <115148099+DimitrisTzalo@users.noreply.github.com>
2025-05-23 13:46:10 +0300

    amhnnnn

--- traineeApp/trainee/src/main/java/myy803/springboot/trainee/controller/ProfessorController.java
+++ traineeApp/trainee/src/main/java/myy803/springboot/trainee/controller/ProfessorController.java
@@ -80,9 +80,7 @@ public class ProfessorController {
         professor.setUser(user);
         professor.setUsername(user.getUsername());
 
-        if (professor.getLoad() == null) {
-            professor.setLoad(0);
-        }
+
 
         professorService.saveProfile(professor);
         model.addAttribute("successMessage", "Profile saved successfully!");
